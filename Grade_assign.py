d8ee495491ddafa160121035938cc037ebe2f22e
Kelly Simmons <s557075@nwmissouri.edu>
2023-11-21 11:56:21 -0500

    Universal Code edit 2

--- Grade_assign.py
+++ Grade_assign.py
@@ -1,11 +1,4 @@
-import pandas as pd
-
-# Assuming you have a DataFrame with a 'Score' column
-data = {'Name': ['Student1', 'Student2', 'Student3'],
-        'Score': [85, 92, 78]}
-df = pd.DataFrame(data)
-
-# Define the assign_grade function
+# Define the grading criteria
 def assign_grade(score):
     if score >= 90:
         return 'A'
@@ -18,8 +11,28 @@ def assign_grade(score):
     else:
         return 'F'
 
-# Apply the assign_grade function to the 'Score' column
-df['Grade'] = df['Score'].apply(assign_grade)
+# Read data from the output file
+file_path = 'path/to/your/output_file.txt'  # Update this with the actual path
+with open(file_path, 'r') as file:
+    lines = file.readlines()
 
-# Display the DataFrame with the assigned grades
-print(df)
+# Process each line and assign grades
+grades = []
+for line in lines:
+    try:
+        score = float(line.strip())  # Assuming the data is numerical
+        grade = assign_grade(score)
+        grades.append(grade)
+    except ValueError:
+        print(f"Skipping invalid data: {line}")
+
+# Display or save the grades as needed
+print("Grades:")
+for i, grade in enumerate(grades, start=1):
+    print(f"Student {i}: {grade}")
+
+# If you want to save the grades to a new file, you can use the following code:
+# with open('path/to/your/grades_file.txt', 'w') as grade_file:
+#     for grade in grades:
+#         grade_file.write(f"{grade}\n")
+
--- Universal_code.py
+++ Universal_code.py
@@ -4,26 +4,75 @@ import os
 # Set the path to the directory containing your Excel files
 input_directory = r'C:\Users\keros\.venv\Capstone-Project'
 
-# List all Excel files in the directory
-excel_files = [f for f in os.listdir(input_directory) if f.endswith('Nursing home Cost_state.xlsx')]
-excel_files = [f for f in os.listdir(input_directory) if f.endswith('Population_stats')]
-excel_files = [f for f in os.listdir(input_directory) if f.endswith('Facility Staffing Combined.xlsx')]
-excel_files = [f for f in os.listdir(input_directory) if f.endswith('State by State Violations2023.xlsx')]
-csv_files = [f for f in os.listdir(input_directory) if f.endswith('Facility_by_state.csv')]
+# List all files in the directory
+all_files = os.listdir(input_directory)
 
+# Specify desired columns for each file
+desired_columns_nursing_home = ['State', 'Region', 'Private Room Annual Cost', 'Shared Room Annual Cost']
+
+desired_columns_occ_rate = ['Occupancy Rate Avg']
+
+desired_columns_population_stats = ['Population 2023']
+
+desired_columns_facility_staffing_combined = ['Staff Average', 'Staff Increase/Decrease']
+
+desired_columns_state_violations = ['Total Deficiencies', 'Fine', 'Sum of Fines']
+
+desired_columns_mapping = {
+    'Nursing home Cost_state.xlsx': desired_columns_nursing_home,
+    'Occ rate 2015_2023.xlsx': desired_columns_occ_rate,
+    'Population_stats.xlsx': desired_columns_population_stats,
+    'Facility Staffing Combined.xlsx': desired_columns_facility_staffing_combined,
+    'State by State Violations2023.xlsx': desired_columns_state_violations
+}
+
+# Filter Excel files
+excel_files = [f for f in all_files if f.endswith('.xlsx')]
+
+# Iterate through each Excel file
+for excel_file in excel_files:
+    excel_path = os.path.join(input_directory, excel_file)
+
+    # Determine which desired_columns list to use based on the file
+    desired_columns = desired_columns_mapping.get(excel_file, [])
+    if not desired_columns:
+        print(f"Desired columns not specified for {excel_file}. Skipping...")
+        continue
+
+    # Read the Excel file with the specified desired_columns
+    df = pd.read_excel(excel_path, usecols=desired_columns)
+
+# Filter CSV files
+csv_files = [f for f in all_files if f.endswith('.csv')]
+
+desired_columns_facility_by_state = ['Facility Total']
 
 # Initialize an empty DataFrame to store the combined data
 combined_data = pd.DataFrame()
 
+# Print the column names in each Excel sheet
+for excel_file in excel_files:
+    excel_path = os.path.join(input_directory, excel_file)
+    df = pd.read_excel(excel_path)
+    print(f"Columns in {excel_file}: {df.columns}")
+
 # Columns you want to include in the final DataFrame
-desired_columns = ['State', 'Population', 'GDP']
+desired_columns = ['State', 'Region', 'Private Room Annual Cost', 'Shared Room Annual Cost',
+                   'Facility Total', 'Population 2023', 'Staff Average ', 'Staff Increase/Decrease',
+                   'Total Deficiencies', 'Fines', 'Sum of Fines']
 
 # Loop through each Excel file and append its data to the combined DataFrame
 for excel_file in excel_files:
+    # Skip temporary Excel files
+    if excel_file.startswith('~$'):
+        continue
+
     excel_path = os.path.join(input_directory, excel_file)
     try:
+        # Print columns before reading
+        print(f"Columns in {excel_file}: {pd.read_excel(excel_path, nrows=0).columns}")
 
-        df = pd.read_excel(excel_path)  # Assumes data is in the first sheet, adjust as needed
+        df = pd.read_excel(excel_path, usecols=desired_columns)  # Assumes data is in the first sheet, adjust as needed
 
         # Check if 'State' column exists
         if 'State' in df.columns:
@@ -37,15 +86,13 @@ for excel_file in excel_files:
     except pd.errors.ParserError as e:
         print(f"Error reading {excel_file}: {e}")
 
-    combined_data = pd.concat([combined_data, df], ignore_index=True)
-
 # Loop through each CSV file and append its data to the combined DataFrame
 for csv_file in csv_files:
     csv_path = os.path.join(input_directory, csv_file)
     try:
-        df = pd.read_csv(csv_path)
+        df = pd.read_csv(csv_path, usecols=desired_columns)
 
-         # Check if 'State' column exists
+        # Check if 'State' column exists
         if 'State' in df.columns:
             # Add State_ID to the DataFrame based on State names
             state_id_mapping = {state: idx + 1 for idx, state in enumerate(df['State'].unique())}
@@ -57,7 +104,6 @@ for csv_file in csv_files:
     except pd.errors.ParserError as e:
         print(f"Error reading {csv_file}: {e}")
 
-        
 # Set the path for the output Excel file
 output_file_path = r'C:\Users\keros\.venv\Capstone-Project\output_data.xlsx'
 
