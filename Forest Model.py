df3db9c08ba243c89485f3ed9db4f03dbaff4e1d
Kelly Simmons <s557075@nwmissouri.edu>
2023-11-26 02:10:45 -0500

    Scatter plot test edit

--- Forest Model.py
+++ Forest Model.py
@@ -1,31 +1,18 @@
 import pandas as pd
-from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestRegressor
-from sklearn.metrics import mean_squared_error
-import numpy as np
+import matplotlib.pyplot as plt
 
-# Load the combined data from outputdata.xlsx
-combined_data = pd.read_excel('C:/Users/keros/.venv/Capstone-Project/outputdata.xlsx')
+# Load your dataset (replace 'imputed_data.xlsx' with your actual file path)
+file_path = r'C:\Users\keros\.venv\Capstone-Project\imputed_data.xlsx'
+df = pd.read_excel(file_path)
 
-# Select relevant columns for modeling
-features = combined_data[['Population 2023']]
-target = combined_data['Annual']
+# Specify the columns for the scatter plot (replace 'Population 2023' and 'Annual' with your desired columns)
+x_column = 'Facility Total'
+y_column = 'Annual'
 
-# Split the data into training and testing sets
-X_train, X_test, y_train, y_test = train_test_split(features, target, test_size=0.2, random_state=42)
-
-# Create and train the random forest model
-rf_model = RandomForestRegressor(n_estimators=100, random_state=42)
-rf_model.fit(X_train, y_train)
-
-# Make predictions on the test set
-y_pred = rf_model.predict(X_test)
-
-# Evaluate the model performance
-mse = mean_squared_error(y_test, y_pred)
-rmse = np.sqrt(mse)
-
-print(f'Mean Squared Error: {mse}')
-print(f'Root Mean Squared Error: {rmse}')
-
-# You can now use rf_model.predict() to make predictions on new data.
+# Create a scatter plot
+plt.scatter(df[x_column], df[y_column])
+plt.title(f'Scatter Plot of {y_column} vs {x_column}')
+plt.xlabel(x_column)
+plt.ylabel(y_column)
+plt.grid(True)
+plt.show()
